b9e8769bb23c400fe055ba60214bd2e94fc621ec
aatif13698 <aatif13698@gmail.com>
2025-05-24 13:03:45 +0530

    socket api changes

--- src/pages/customField/ListForm.jsx
+++ src/pages/customField/ListForm.jsx
@@ -15,7 +15,7 @@ import io from 'socket.io-client';
 import { useSelector } from 'react-redux';
 import { v4 as uuidv4 } from 'uuid';
 
-// test
+// test qqq
 
 console.log("VITE_SOCKET_API_URL",import.meta.env.VITE_SOCKET_API_URL);
 
