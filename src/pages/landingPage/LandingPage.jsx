a3be813bbdf542e3b09cc010e7204e7af948412f
aatif13698 <aatif13698@gmail.com>
2025-06-12 13:55:04 +0530

    landing page starter

--- src/pages/landingPage/LandingPage.jsx
+++ src/pages/landingPage/LandingPage.jsx
@@ -1,14 +1,27 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom';
 
 function LandingPage({ companyIdentifier }) {
 
-    console.log("companyIdentifier landing",companyIdentifier);
-    
-  return (
-    <div>
-      This is landig page
-    </div>
-  )
+    const navigate = useNavigate()
+    console.log("companyIdentifier landing", companyIdentifier);
+    useEffect(() => {
+        if (import.meta.env.VITE_NODE_ENV == "development") {
+            if (companyIdentifier && !companyIdentifier == "aestree") {
+                navigate("/login")
+            }
+        } else {
+            if (companyIdentifier && !companyIdentifier == "8081") {
+                navigate("/login")
+            }
+        }
+    }, [companyIdentifier])
+
+    return (
+        <div>
+            This is landig page
+        </div>
+    )
 }
 
 export default LandingPage
