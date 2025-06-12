9a6520eab21aee8abdf7cd6eb2595b1a6a4c3959
aatif13698 <aatif13698@gmail.com>
2025-06-12 14:41:25 +0530

    landing page route and validation

--- src/pages/landingPage/LandingPage.jsx
+++ src/pages/landingPage/LandingPage.jsx
@@ -2,16 +2,19 @@ import React, { useEffect } from 'react'
 import { useNavigate } from 'react-router-dom';
 
 function LandingPage({ companyIdentifier }) {
+    console.log("import.meta.env.VITE_NODE_ENV", import.meta.env.VITE_NODE_ENV);
+
 
     const navigate = useNavigate()
     console.log("companyIdentifier landing", companyIdentifier);
     useEffect(() => {
         if (import.meta.env.VITE_NODE_ENV == "development") {
-            if (companyIdentifier && !companyIdentifier == "aestree") {
+            if (companyIdentifier && !companyIdentifier == "8081") {
                 navigate("/login")
             }
         } else {
-            if (companyIdentifier && !companyIdentifier == "8081") {
+
+            if (companyIdentifier && !companyIdentifier == "aestree") {
                 navigate("/login")
             }
         }
